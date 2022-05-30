fcbdd8a77bbefa7c9390baab48cb0ba8b61c5eff
aa2akhtech <97241913+aa2akhtech@users.noreply.github.com>
2022-05-30 09:40:03 +0530

    Update Content.js

--- src/client/containers/LandingPage/Content.js
+++ src/client/containers/LandingPage/Content.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+import React, { useState } from 'react';
+import Alert from '../components/Alert';
 // import PropTypes from 'prop-types'
 
 const Content = () => (
@@ -86,6 +87,84 @@ const Content = () => (
       <div className="banner-main-content">
         <h2>GET THE INDIA's LATEST BREAKING NEWS</h2>
         <h3>India's Leading Breaking News Portal</h3>
+  export default function Upload() {
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewSource, setPreviewSource] = useState('');
+    const [selectedFile, setSelectedFile] = useState();
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        previewFile(file);
+        setSelectedFile(file);
+        setFileInputState(e.target.value);
+    };
+
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewSource(reader.result);
+        };
+    };
+
+    const handleSubmitFile = (e) => {
+        e.preventDefault();
+        if (!selectedFile) return;
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = () => {
+            uploadImage(reader.result);
+        };
+        reader.onerror = () => {
+            console.error('AHHHHHHHH!!');
+            setErrMsg('something went wrong!');
+        };
+    };
+
+    const uploadImage = async (base64EncodedImage) => {
+        try {
+            await fetch('/api/upload', {
+                method: 'POST',
+                body: JSON.stringify({ data: base64EncodedImage }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            setFileInputState('');
+            setPreviewSource('');
+            setSuccessMsg('Image uploaded successfully');
+        } catch (err) {
+            console.error(err);
+            setErrMsg('Something went wrong!');
+        }
+    };
+    return (
+        <div>
+            <h1 className="title">Upload an Image</h1>
+            <Alert msg={errMsg} type="danger" />
+            <Alert msg={successMsg} type="success" />
+            <form onSubmit={handleSubmitFile} className="form">
+                <input
+                    id="fileInput"
+                    type="file"
+                    name="image"
+                    onChange={handleFileInputChange}
+                    value={fileInputState}
+                    className="form-input"
+                />
+                <button className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
+            {previewSource && (
+                <img
+                    src={previewSource}
+                    alt="chosen"
+                    style={{ height: '300px' }}
+                />
+            )}
+        </div>
+    );
+}
 
         <button>
           <a href="#">Know More</a>
